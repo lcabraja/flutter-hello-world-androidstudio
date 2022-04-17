3eb2c91cae3440b0daeec29edb4680de934c32e1
Luka Cabraja <luka@cabraja.eu>
2022-04-17 13:22:03 +0200

    step 4
    https://codelabs.developers.google.com/codelabs/first-flutter-app-pt2#3

--- lib/main.dart
+++ lib/main.dart
@@ -30,6 +30,7 @@ class RandomWords extends StatefulWidget {
 
 class _RandomWordsState extends State<RandomWords> {
   final _suggestions = <WordPair>[];
+  final _saved = <WordPair>{};
   final _biggerFont = const TextStyle(fontSize: 18.0);
 
   @override
@@ -40,17 +41,23 @@ class _RandomWordsState extends State<RandomWords> {
         padding: const EdgeInsets.all(16.0),
         itemBuilder: /*1*/ (context, i) {
           if (i.isOdd) return const Divider();
-          /*2*/
 
+          /*2*/
           final index = i ~/ 2; /*3*/
           if (index >= _suggestions.length) {
             _suggestions.addAll(generateWordPairs().take(10)); /*4*/
           }
+          final alreadySaved = _saved.contains(_suggestions[index]);
 
           return ListTile(
             title: Text(
               _suggestions[index].asPascalCase,
               style: _biggerFont,
+            ),
+            trailing: Icon(
+              alreadySaved ? Icons.favorite : Icons.favorite_border,
+              color: alreadySaved ? Colors.red : null,
+              semanticLabel: alreadySaved ? 'Remove from saved' : 'Save',
             ),
           );
         },
